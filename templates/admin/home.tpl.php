5bda2166ceb2afc815b62cd6238197b076e548a3
fotofacil <fotofacil@live.com>
2020-01-27 18:10:52 -0300

    problema da notificacao resolvido

--- templates/admin/home.tpl.php
+++ templates/admin/home.tpl.php
@@ -1,6 +1,2 @@
-<?php include __DIR__.'/../../templates/BASE/head.php'?>
-
     <h3>pagina inicial template ADMIN</h3>
     <a href="/admin/pages">Admin Pages</a>
-
-<?php include __DIR__.'/../../templates/BASE/footer.php'?>
--- templates/admin/pages.tpl.php
+++ templates/admin/pages.tpl.php
@@ -1,6 +1,5 @@
-<?php include __DIR__.'/../../templates/BASE/head.php'?>
+
 
     <h2>admin pages</h2>
     <a href="/admin">Admin Home</a>
 
-<?php include __DIR__.'/../../templates/BASE/footer.php'?>
